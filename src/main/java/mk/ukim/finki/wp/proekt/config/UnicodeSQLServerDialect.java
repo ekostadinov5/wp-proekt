9ded4678b7a2d25a15d846aa41d84e7040aef4b7
Evgenij Kostadinov <ekostadinov5@gmail.com>
2020-02-11 19:20:47 +0100

    Switch to MS SQL Server

---
+++ src/main/java/mk/ukim/finki/wp/proekt/config/UnicodeSQLServerDialect.java
@@ -0,0 +1,25 @@
+package mk.ukim.finki.wp.proekt.config;
+
+import org.hibernate.dialect.SQLServerDialect;
+
+import java.sql.Types;
+
+/**
+ * Unicode support in SQL Server
+ */
+public class UnicodeSQLServerDialect extends SQLServerDialect {
+
+    public UnicodeSQLServerDialect() {
+        super();
+
+        // Use Unicode Characters
+        registerColumnType(Types.VARCHAR, 255, "nvarchar($l)");
+        registerColumnType(Types.CHAR, "nchar(1)");
+        registerColumnType(Types.CLOB, "nvarchar(max)");
+
+        // Microsoft SQL Server 2000 supports bigint and bit
+        registerColumnType(Types.BIGINT, "bigint");
+        registerColumnType(Types.BIT, "bit");
+    }
+
+}
